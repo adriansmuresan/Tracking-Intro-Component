8b1eaea38db59523a9a5120862a8182c689256d9
Adrian S MUresan <adriansmuresan@gmail.com>
2020-07-31 15:10:13 -0500

    Minor adjustments

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const hamburger = document.getElementById('hamburger');
 const { body } = document;
 
 hamburger.addEventListener('click', (e) => {
-  e.preventDefault();
-  
   body.classList.toggle('show-nav');
 });
